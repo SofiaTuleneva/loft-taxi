280d5928bf4db5ffbcd67228d475b0a9613b072b
sofia <sofiatuleneva@gmail.com>
2020-01-19 18:47:32 +0300

    changePage as param

--- src/App.js
+++ src/App.js
@@ -6,6 +6,29 @@ import Profile from './profile/Profile';
 import Signup from './signup/Signup';
 import Header from './shared/Header';
 
+const PAGES = [
+    {
+        id: 'login',
+        name: 'Вход',
+        component: changePage => <Login changePage={changePage} />
+    },
+    {
+        id: 'map',
+        name: 'Карта',
+        component: () => <Map />
+    },
+    {
+        id: 'profile',
+        name: 'Профиль',
+        component: () => <Profile />
+    },
+    {
+        id: 'signup',
+        name: 'Регистрация',
+        component: changePage => <Signup changePage={changePage} />
+    }
+];
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -14,29 +37,6 @@ class App extends React.Component {
         };
     }
 
-    PAGES = [
-        {
-            id: 'login',
-            name: 'Вход',
-            component: () => <Login changePage={this.changePage} />
-        },
-        {
-            id: 'map',
-            name: 'Карта',
-            component: () => <Map />
-        },
-        {
-            id: 'profile',
-            name: 'Профиль',
-            component: () => <Profile />
-        },
-        {
-            id: 'signup',
-            name: 'Регистрация',
-            component: () => <Signup changePage={this.changePage} />
-        }
-    ];
-
     changePage = pageId => {
         this.setState({
             activePageId: pageId
@@ -44,16 +44,16 @@ class App extends React.Component {
     };
 
     getPageData = () => {
-        return this.PAGES.find(el => el.id === this.state.activePageId);
+        return PAGES.find(el => el.id === this.state.activePageId);
     };
 
     render() {
-        const activeComponent = this.getPageData().component();
+        const activeComponent = this.getPageData().component(this.changePage);
 
         return (
             <div className="App">
                 <Header 
-                    pages={this.PAGES}
+                    pages={PAGES}
                     changePage={this.changePage}
                     activePageId={this.state.activePageId}
                 />
