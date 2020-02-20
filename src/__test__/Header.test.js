77010256c6bc9942fd2f9c1ec4420951aa09a8ba
sofia <sofiatuleneva@gmail.com>
2020-02-20 07:16:55 +0300

    Tests

--- src/__test__/Header.test.js
+++ src/__test__/Header.test.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import Header from '../components/Header';
+import { Provider } from 'react-redux'
+import {render} from '@testing-library/react';
+import configureStore from 'redux-mock-store'
+import {BrowserRouter} from 'react-router-dom';
 
 jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
 	Map: () => ({})
 }));
+const initialState = {};
+const mockStore = configureStore();
+let store;
+store = mockStore(initialState);
 
 it("Header renders correctly", () => {
 	const setPage = jest.fn();
-	const component = renderer.create(
-		<Header
+
+	const component = render(
+		<Provider store={store}><BrowserRouter><Header
 			setPage={setPage}
 			activePageId={'login'}
 			pages={[]}
-		/>,
+		/></BrowserRouter></Provider>,
 	);
-	let tree = component.toJSON();
-	expect(tree).toMatchSnapshot();
+// console.log('component', component);
+// 	let tree = component.toJSON();
+	expect(component).toMatchSnapshot();
 });
--- src/__test__/Map.test.js
+++ src/__test__/Map.test.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Map from '../components/Map';
+import configureStore from 'redux-mock-store'
+import {Provider} from "react-redux";
+import {BrowserRouter} from 'react-router-dom';
 
 jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
 	Map: () => ({})
 }));
+const initialState = {};
+const mockStore = configureStore();
+let store;
+store = mockStore(initialState);
 
 it("Map renders correctly", () => {
 	const component = renderer.create(
-		<Map />,
+		<Provider store={store}><BrowserRouter><Map /></BrowserRouter></Provider>,
 	);
 	let tree = component.toJSON();
 	expect(tree).toMatchSnapshot();
